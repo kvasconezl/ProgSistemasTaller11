db290b8f199879d6b6d0f1a40653a344dbd83af0
Roberth Loor <rjloor@espol.edu.ec>
2017-07-27 16:24:23 -0500

    funcion genera arreglo

--- hilos.c
+++ hilos.c
@@ -10,42 +10,46 @@ int aleatorio(int min, int max){
 	
 }
 
+int *generaArreglo(int size){
+	int *arreglo;
+	arreglo = (int*) malloc(size*sizeof(int));
+	int i;
+	srand(time(0));
+	
+	for (i = 0; i < size; i++)
+	{
+		int valor=aleatorio(0,200);
+		arreglo[i] = valor;
+	}
+
+	return arreglo;
+	
+}
+
 double obtenerTiempoActual(){
 	struct timespec tsp;
 
-	clock_gettime( CLOCK_REALTIME, &tsp);
+	clock_gettime(CLOCK_REALTIME, &tsp);
 
 	double secs = (double)tsp.tv_sec;
 	double nano = (double)tsp.tv_nsec / 1000000000.0;
 
 	return secs + nano;
 }
-typedef struct mi_estructuraHilo{
-	int * arreglo;
-	int inicio;
-	int final;
-} estructuraH;
-
-void * funcion_hilo_suma(void *arg){
-	
-	estructuraH *argumentos = (estructuraH *)arg;	
-
-	int i = 0;
-	long suma = 0;
-
-	for(i = argumentos->inicio; i < argumentos->final; i++){
-		suma = suma + (argumentos->arreglo)[i];
-	}
-	
-	return (void *)suma;
-
-}
 
 int main(){
 	srand(time(0));
 	printf("Tiempo primero %.9f\n", obtenerTiempoActual());
-	printf("Aleatorio %d\n", aleatorio(1,200));
+	printf("Aleatorio %d\n", aleatorio(7,200));
 	printf("Tiempo segundo %.9f\n", obtenerTiempoActual());
+	
+	int *a=generaArreglo(5);
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d\n",a[i] );
+	}
+
 	
 	
 	return 1;
